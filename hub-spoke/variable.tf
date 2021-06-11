0893b465c2df14def77a17b2c7f9956a11754474
Yi Zhang <misteryiz@gmail.com>
2021-06-10 21:36:03 -0500

    initial commit

---
+++ hub-spoke/variable.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  description = "Location of the network"
+  default     = "eastus"
+}
+
+variable "username" {
+  description = "Username for Virtual Machines"
+  default     = "azureuser"
+}
+
+variable "password" {
+  description = "Password for Virtual Machines"
+  default     = "Password1234!"
+}
+
+variable "vmsize" {
+  description = "Size of the VMs"
+  default     = "Standard_DS1_v2"
+}
